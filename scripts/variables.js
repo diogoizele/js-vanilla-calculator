a4e9de88e6ef84f35f0a895346cc38b070dde35b
diogoizele <diogogabrielizele@gmail.com>
2023-03-16 23:46:42 -0300

    feat: add variables declaration 😝

--- scripts/variables.js
+++ scripts/variables.js
@@ -2,3 +2,8 @@ const operators = Array.from(document.querySelectorAll("[data-operator]"));
 const numbers = Array.from(document.querySelectorAll("[data-number]"));
 const display = document.querySelector("[data-display]");
 
+const varValue = document.querySelector("[data-value]");
+const varAccumulator = document.querySelector("[data-accumulator]");
+const varResult = document.querySelector("[data-result]");
+const varOperation = document.querySelector("[data-operation]");
+
